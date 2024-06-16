ee73511f05284da28554361a00bd48925b47a904
AnshAggarwalMAC <51926978+anshaggarwal28@users.noreply.github.com>
2024-06-16 18:09:10 -0400

    1st
    lecture updates

---
+++ lecture_6_Practical_intro_to_js/1_intro.js
@@ -0,0 +1,46 @@
+/**
+ * variable declaration
+ * simple how js runs the code
+ * dynamically typed language
+ */
+
+
+console.log("Inside Intro");
+
+// // variable declaration -> varName is a variable that currently holds undefined value
+// let varName; 
+// // default value of variable is undefined
+
+// console.log("7",varName);
+// varName=10; 
+// console.log("9",varName);
+// varName=10.1; 
+// console.log("11",varName); 
+
+
+// //strings in  js we only have strings
+// let newVar; 
+
+// newVar= "I am a String"; 
+// console.log("18",newVar); 
+
+// //boolean
+
+// newVar= true; 
+// console.log("23",newVar)
+
+// //null, undefined means no value exist in this variable. 
+// newVar = null ; 
+// console.log(newVar); 
+
+/**
+ * how to know the current type of value that a  variable holds. 
+ * 1. there is type of variable
+ */
+let varName; 
+varName= 10; // number
+varName="hello"; // string
+varName= false; // boolean
+varName=10.1;// number
+varName= 'hello'; //string
+console.log(typeof varName); 
