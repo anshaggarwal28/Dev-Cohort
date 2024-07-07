c490f09109d32bc939da0025550f8cd2b3ef1216
AnshAggarwalMAC <51926978+anshaggarwal28@users.noreply.github.com>
2024-07-06 20:57:47 -0400

    update notes

---
+++ lecture_9_async_eventloop/2_sync_async.js
@@ -0,0 +1,32 @@
+// // Ques -1 
+
+// /***
+//  * 
+//  * Synchrous - the code that is executed line by line.
+//  * 
+//  */
+
+// console.log("before"); 
+
+// function fn() {
+//     console.log("I am fn") ; 
+// }
+
+// fn() ; 
+// console.log("After") ; 
+
+
+// Ques-2
+/**
+ * Asynchronous code : a part of code is executed 
+ * first and another section of code is delayed 
+ * and that part is executed later on.
+ */
+//1
+console.log("Before") ; 
+setTimeout(fn,2000) ; 
+function fn() {
+    console.log("I am fn") ; 
+}
+
+console.log("After") ; 
